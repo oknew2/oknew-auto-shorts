db3e1addeee654244600026b0407a01fd0c5d85c
oknew2 <tekwon1000@gmail.com>
2025-05-12 19:56:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-﻿import os
+import os
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 youtube_client_id = os.getenv("YOUTUBE_CLIENT_ID")
 youtube_client_secret = os.getenv("YOUTUBE_CLIENT_SECRET")
 youtube_refresh_token = os.getenv("YOUTUBE_REFRESH_TOKEN")
 heygen_token = os.getenv("HEYGEN_API_TOKEN")
-intro_video_url = os.getenv("INTRO_VIDEO_URL")
-
+intro_video_url = os.getenv("GOOGLE_DRIVE_INTRO_VIDEO_URL")
 
 print("OpenAI API key:", openai_api_key)
 print("YouTube Upload 준비 완료")
 print("인트로 영상 URL:", intro_video_url)
-
